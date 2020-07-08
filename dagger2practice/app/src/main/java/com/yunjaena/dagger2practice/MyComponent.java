08887a19ffeda274771fac332ce2fcd4464e0418
Yunjae-Na <yunjaena@gmail.com>
2020-07-08 18:55:06 +0900

    Add MyComponent interface
    Add MyComponent interface

---
+++ dagger2practice/app/src/main/java/com/yunjaena/dagger2practice/MyComponent.java
@@ -0,0 +1,8 @@
+package com.yunjaena.dagger2practice;
+
+import dagger.Component;
+
+@Component(modules = MyModule.class)
+public interface MyComponent {
+    String getString(); //프로비전 메서드, 바인드된 모듈로부터 의존성을 제공
+}
