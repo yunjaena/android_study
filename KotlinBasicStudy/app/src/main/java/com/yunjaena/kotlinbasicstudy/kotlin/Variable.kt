435562248d97402754beae3b14923bfc3b1ca867
Yunjae-Na <yunjaena@gmail.com>
2020-07-26 02:08:45 +0900

    Add Variable kotlin file
    aAdd Variable kotlin file

---
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/Variable.kt
@@ -0,0 +1,17 @@
+package com.yunjaena.kotlinbasicstudy.kotlin
+
+// 01. Variable => 변수
+// 상자
+//            - 내 마음대로 원하는 것을 넣을 수 있는 상자 => Variable
+//            - 한번 넣으면 바꿀 수 있는 상자 => Value
+
+// 변수 선언하는 방법
+// var/val 변수명(상자) = 값(넣고 싶은 것)
+// Variable/Value
+var num = 10
+var hello = "hello"
+var point = 3.4
+
+fun main(args:Array<String>){
+
+}
