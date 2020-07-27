22e349cdf30e4655ee555ce8a67a7c1aa4a796f1
Yunjae-Na <yunjaena@gmail.com>
2020-07-27 17:02:37 +0900

    Add kotlin method exmple
    Add kotlin method exmple

--- KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/04.Function.kt
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/04.Function.kt
@@ -19,6 +19,34 @@ fun plus(first: Int, second: Int): Int {
     return result
 }
 
+// - 디폴트 값을 갖는 함수 만들기
+
+fun plusFive(first: Int, second: Int = 5): Int {
+    val result: Int = first + second
+    return result
+}
+
+// - 반환값이 없는 함수 만들기
+fun printPlus(first: Int, second: Int): Unit {
+    val result: Int = first + second
+    println(result)
+}
+
+fun printPlus2(first: Int, second: Int) {
+    val result: Int = first + second
+    println(result)
+}
+
+// - 간단하게 함수를 선언하는 방법
+fun plusShort(first: Int, second: Int) = first + second
+
+// - 가변인자를 갖는 함수 선언하는 방법
+fun plusMany(vararg numbers: Int) {
+    for (number in numbers) {
+        println(number)
+    }
+}
+
 fun main(array: Array<String>) {
     // 함수를 호출하는 방법
     val result = plus(5, 10)
@@ -30,4 +58,22 @@ fun main(array: Array<String>) {
 
     val result3 = plus(second = 100, first = 10)
     println(result3)
+
+    // 디폴트 값을 갖는 함수 호출하기
+    println()
+    val result4 = plusFive(10, 20)
+    println(result4)
+    val result5 = plusFive(10)
+    println(result5)
+
+    println()
+    printPlus(10, 20)
+
+    println()
+    val result6 = plusShort(50, 50)
+    println(result6)
+
+    println()
+    plusMany(1, 2, 3)
+    plusMany(100)
 }
