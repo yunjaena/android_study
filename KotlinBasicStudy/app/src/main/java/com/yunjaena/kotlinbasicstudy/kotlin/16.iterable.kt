7cbc2a05f6d8823b04f00ecd410c5ddc95afdfd7
Yunjae-Na <yunjaena@gmail.com>
2020-07-29 22:21:50 +0900

    Add kotlin second iterable example
    Add kotlin second iterable example

--- KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/16.iterable.kt
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/16.iterable.kt
@@ -3,22 +3,74 @@ package com.yunjaena.kotlinbasicstudy.kotlin
 fun main(array: Array<String>) {
     val a = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-    // 반복하는 방법(1)
-    for(item in a){
-        if(item == 5)
+    // 반복하는 방법 (1)
+    for (item in a) {
+        if (item == 5)
             println("item is Five")
         else
             println("item is not Five")
     }
     println()
 
-    // 반복하는 방법(2)
-    for((index, item) in a.withIndex()){
+    // 반복하는 방법 (2)
+    for ((index, item) in a.withIndex()) {
         println("index : " + index + " value : " + item)
         // 문자열 + Int(정수) = 문자열
         // 문자열 + 아무거나 =  문자열
     }
 
+    println()
 
+    // 반복하는 방법 (3)
+    a.forEach {
+        println(it)
+    }
 
+    // 반복하는 방법 (4)
+    a.forEach { item ->
+        println(item)
+    }
+    println()
+
+    // 반복하는 방법 (5)
+    a.forEachIndexed { index, item ->
+        println("index : " + index + " value : " + item)
+    }
+    println()
+
+    // 반복하는 방법 (6)
+    println(a.size)
+    for (i in 0 until a.size) {
+        // until은 마지막을 포함 하지 않는다.
+        // 0 부터 8 까지 이다.
+        println(a.get(i))
+    }
+
+    println()
+
+    // 반복하는 방법 (7)
+    for (i in 0 until a.size step (2)) {
+        println(a.get(i))
+    }
+    println()
+
+    // 반복하는 방법 (8)
+    for (i in a.size - 1 downTo (0)) {
+        // 8 부터 0 까지 반복
+        println(a.get(i))
+    }
+    println()
+
+    //  반복하는 방법 (9)
+    for (i in a.size - 1 downTo (0) step (2)) {
+        // 8 부터 0 까지 반복
+        println(a.get(i))
+    }
+    println()
+
+    //  반복하는 방법 (10)
+    for(i in 0 .. a.size){
+        // .. 은 마지막을 포함한다.
+        println(i)
+    }
 }
