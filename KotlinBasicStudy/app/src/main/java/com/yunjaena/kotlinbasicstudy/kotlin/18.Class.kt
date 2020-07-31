b7620f3d143a374874d439d197f9734b9c72dbe3
Yunjae-Na <yunjaena@gmail.com>
2020-07-31 10:47:13 +0900

    Add kotlin second class example
    Add kotlin second class example

--- KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/18.Class.kt
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/18.Class.kt
@@ -16,5 +16,36 @@ package com.yunjaena.kotlinbasicstudy.kotlin
 // 객체를 만드는 방법
 // - 설명서가 있어야 한다.
 
-fun main(args : Array<String>){
+fun main(args: Array<String>) {
+    // 클래스(설명서)를 통해서 실체를 만드는 방법
+    // -> 인스턴스화 -> 인스턴스(객체)
+    Car("v8 engine", "big")
+
+    val bigCar = Car("v8 engine", "big")
+
+    // 우리가 만든 클래스(설명서)는 자료형이 된다.
+    val bigCar1 : Car = Car("v8 engine", "big")
+    val superCar : SuperCar = SuperCar("good engine", "big", "white")
+
 }
+
+// 클래스(설명서) 만드는 방법 (1)
+class Car(var engine: String, var body: String) {
+}
+
+// 클래스(설명서) 만드는 방법 (2)
+class SuperCar {
+    var engine : String
+    var body : String
+    var door  : String
+
+    constructor(engine: String, body: String, door: String){
+        println(engine)
+        println(body)
+        println(door)
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+}
+
