1f22df2acdc0357887b3d486f12c080cc5d330e3
Yunjae-Na <yunjaena@gmail.com>
2020-07-31 10:21:03 +0900

    Add kotlin first class example
    Add kotlin first class example

---
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/18.Class.kt
@@ -0,0 +1,20 @@
+package com.yunjaena.kotlinbasicstudy.kotlin
+
+// 18. Class
+// OOP -> Object Oriented Programing (객체지향 프로그래밍)
+
+// "객체"란 뭘까?
+// - 이름이 있는 모든 것
+
+// 절차지향 프로그래밍
+// - 코드를 위해서 부터 아래로 실행을 하면 문제가 해결된다!
+
+// 객체지향 프로그래밍
+// - 객체를 만들어서, 객체에게 일을 시켜서 문제를 해결한다.
+// - 선수, 심판, 경기장, 관중 -> 축구 게임
+
+// 객체를 만드는 방법
+// - 설명서가 있어야 한다.
+
+fun main(args : Array<String>){
+}
