1dc08bfc00eb7274ee3c9321a84b846c3b3c6858
Yunjae-Na <yunjaena@gmail.com>
2020-08-02 02:20:36 +0900

    Add inherit second exmple
    Add inherit second exmple

--- KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/25.Inherit.kt
+++ KotlinBasicStudy/app/src/main/java/com/yunjaena/kotlinbasicstudy/kotlin/25.Inherit.kt
@@ -8,19 +8,19 @@ package com.yunjaena.kotlinbasicstudy.kotlin
 
 fun main(args : Array<String>){
 val superCar100 : SuperCar100 = SuperCar100()
-    superCar100.drive()
+    println(superCar100.drive())
     superCar100.stop()
 
     val bus100 : Bus100 = Bus100()
-//    bus100.drive()
+    bus100.drive()
 }
 
-// class => 기본 private , 변수 => 기본 public
+// class => 기본 private , 변수 => 기본 public, 함수 => 기본 private
 // 부모 : Car100
 // 자식 : SuperCar100, Bus100
 open class Car100(){
-    fun drive(){
-
+    open fun drive() : String{
+        return  "달린다"
     }
     fun stop(){
 
@@ -28,9 +28,13 @@ open class Car100(){
 }
 
 class SuperCar100() : Car100(){
+    override fun drive() : String{
+        val run = super.drive()
+        return "빨리 $run"
+    }
 
 }
 
-class Bus100(){
+class Bus100() : Car100(){
 
 }
