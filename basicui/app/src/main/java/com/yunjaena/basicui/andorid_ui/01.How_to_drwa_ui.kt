2c8c6d92d1eebbc937cd627ac7eb4a1478ca6bc9
Yunjae-Na <yunjaena@gmail.com>
2020-08-02 21:51:43 +0900

    Add android basic ui project
    Add android basic ui project

---
+++ basicui/app/src/main/java/com/yunjaena/basicui/andorid_ui/01.How_to_drwa_ui.kt
@@ -0,0 +1,7 @@
+package com.yunjaena.basicui.andorid_ui
+
+// 안드로이드에서 화면을 그리는 방법
+// - XML을 이용한다.
+// - XML은 뭘까
+//      - DSL Language -> Domain Specific Language
+//      - 안드로이드 UI를 그리기 위해 특화된 언어이다
