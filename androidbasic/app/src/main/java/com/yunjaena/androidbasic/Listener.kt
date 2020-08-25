766f74756397fc52f33b1b00fb3a043c9ee824f6
Yunjae-Na <yunjaena@gmail.com>
2020-08-25 14:20:50 +0900

    Add android listener practice
    Add android listener practice

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/Listener.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/Listener.kt
@@ -1,8 +1,9 @@
 package com.yunjaena.androidbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_listener.*
 
 class Listener : AppCompatActivity() {
@@ -15,5 +16,27 @@ class Listener : AppCompatActivity() {
         // val textView : TextView = findViewById(R.id.hello)
         // 2) xml을 import해서 가져온다.
         // hello
+
+        // 익명 함수
+        // 1 -> 람다 방식
+        hello.setOnClickListener {
+            Log.d("click", "Click!!")
+        }
+
+        // 2 -> 익명 함수 방식
+        hello.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("click", "Click!!")
+            }
+        })
+
+        // 3 -> 이름이 필요한 경우 (click)
+        val click = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Log.d("click", "Click!!")
+            }
+        }
+
+        hello.setOnClickListener(click)
     }
 }
