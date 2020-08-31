4f38b54d77739d39881b67cb440f2bfe62f5b2dd
Yunjae-Na <yunjaena@gmail.com>
2020-08-31 15:56:28 +0900

    Add first android intent example
    Add first android intent example

---
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent1.kt
@@ -0,0 +1,18 @@
+package com.yunjaena.androidbasic
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intent.*
+
+class Intent1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+        change_activity.setOnClickListener{
+            val intent = Intent(this@Intent1, Intent2::class.java)
+            startActivity(intent)
+        }
+    }
+}
