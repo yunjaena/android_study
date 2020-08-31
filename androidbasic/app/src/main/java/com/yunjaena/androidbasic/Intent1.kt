21d78de25d06e8e718ce9de9d8d08bb76aa54a62
Yunjae-Na <yunjaena@gmail.com>
2020-08-31 17:33:14 +0900

    Add android intent fourth example
    Add android intent fourth example

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent1.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent1.kt
@@ -1,6 +1,7 @@
 package com.yunjaena.androidbasic
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -12,20 +13,21 @@ class Intent1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
         change_activity.setOnClickListener {
-            val intent = Intent(this@Intent1, Intent2::class.java)
-            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다. -> Dictionary
-            intent.putExtra("number1", 1)
-            intent.putExtra("number2", 2)
-            //startActivity(intent)
-
-            val intent2 = Intent(this@Intent1, Intent2::class.java)
-            // Apply ->
-            intent2.apply {
-                putExtra("number1", 1)
-                putExtra("number2", 2)
-                startActivityForResult(this, 200)
-            }
-
+//            val intent = Intent(this@Intent1, Intent2::class.java)
+//            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다. -> Dictionary
+//            intent.putExtra("number1", 1)
+//            intent.putExtra("number2", 2)
+//            //startActivity(intent)
+//
+//            val intent2 = Intent(this@Intent1, Intent2::class.java)
+//            // Apply ->
+//            intent2.apply {
+//                putExtra("number1", 1)
+//                putExtra("number2", 2)
+//                startActivityForResult(this, 200)
+//            }
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"))
+            startActivity(intent)
         }
     }
 
