8cae33ce125be679a899045427d90abdede95d0e
Yunjae-Na <yunjaena@gmail.com>
2020-08-31 16:31:56 +0900

    Add android intent second example
    Add android intent second example

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent1.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent1.kt
@@ -1,8 +1,8 @@
 package com.yunjaena.androidbasic
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_intent.*
 
 class Intent1 : AppCompatActivity() {
@@ -10,9 +10,21 @@ class Intent1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
-        change_activity.setOnClickListener{
+        change_activity.setOnClickListener {
             val intent = Intent(this@Intent1, Intent2::class.java)
-            startActivity(intent)
+            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다. -> Dictionary
+            intent.putExtra("number1", 1)
+            intent.putExtra("number2", 2)
+            //startActivity(intent)
+
+            val intent2 =  Intent(this@Intent1, Intent2::class.java)
+            // Apply ->
+            intent2.apply {
+                putExtra("number1", 1)
+                putExtra("number2", 2)
+                startActivity(this)
+            }
+            
         }
     }
 }
--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent2.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/Intent2.kt
@@ -1,12 +1,17 @@
 package com.yunjaena.androidbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class Intent2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent2)
+        val number1 = intent.getIntExtra("number1", 0)
+        val number2 = intent.getIntExtra("number2", 0)
+        Log.d("number1", number1.toString())
+        Log.d("number2", number2.toString())
     }
 }
