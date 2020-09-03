cffd09f24e7ef21fa2ebf42a509748d51c9f95e9
Yunjae-Na <yunjaena@gmail.com>
2020-09-04 01:40:47 +0900

    Add android fragment fourth practice
    Add android fragment fourth practice

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/FragmentOne.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/FragmentOne.kt
@@ -7,15 +7,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_one.*
 
 class FragmentOne : Fragment() {
+    interface OnDataPassListener {
+        fun onDataPass(data: String?)
+    }
+
+    lateinit var dataPassListener: OnDataPassListener
+
     override fun onAttach(context: Context) {
-        Log.d("life_cycle","F onAttach")
+        Log.d("life_cycle", "F onAttach")
         super.onAttach(context)
+//        if (context is OnDataPassListener)
+//            dataPassListener = context
+        dataPassListener =  context as OnDataPassListener
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("life_cycle","F onCreate")
+        Log.d("life_cycle", "F onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -27,49 +37,54 @@ class FragmentOne : Fragment() {
         // 프라그먼트가 인터페이스를 처음으로 그릴 때 호출된다.
         // inflater -> 뷰를 그려주는 역할
         // container -> 부모뷰
-        Log.d("life_cycle","F onCreateView")
+        Log.d("life_cycle", "F onCreateView")
         return inflater.inflate(R.layout.fragment_one, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("life_cycle","F onViewCreated")
+        Log.d("life_cycle", "F onViewCreated")
         super.onViewCreated(view, savedInstanceState)
+
+        // Activity의 onCreate에서 했던 작업을 여기에서 한다.
+        pass.setOnClickListener {
+                dataPassListener.onDataPass("Good Bye")
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d("life_cycle","F onActivityCreated")
+        Log.d("life_cycle", "F onActivityCreated")
         val data = arguments?.getString("hello")
         Log.d("data", data)
         super.onActivityCreated(savedInstanceState)
     }
 
     override fun onStart() {
-        Log.d("life_cycle","F onStart")
+        Log.d("life_cycle", "F onStart")
         super.onStart()
     }
 
     override fun onResume() {
-        Log.d("life_cycle","F onResume")
+        Log.d("life_cycle", "F onResume")
         super.onResume()
     }
 
     override fun onPause() {
-        Log.d("life_cycle","F onPause")
+        Log.d("life_cycle", "F onPause")
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d("life_cycle","F onStop")
+        Log.d("life_cycle", "F onStop")
         super.onStop()
     }
 
     override fun onDestroyView() {
-        Log.d("life_cycle","F onDestroyView")
+        Log.d("life_cycle", "F onDestroyView")
         super.onDestroyView()
     }
 
     override fun onDetach() {
-        Log.d("life_cycle","F onDetach")
+        Log.d("life_cycle", "F onDetach")
         super.onDetach()
     }
 }
