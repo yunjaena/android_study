e592fbac7c42fcddd746eae27f27d0b33aea2042
Yunjae-Na <yunjaena@gmail.com>
2020-09-04 02:15:30 +0900

    Add android lateinit explain
    Add android lateinit explain

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/NullSafety.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/NullSafety.kt
@@ -5,10 +5,18 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class NullSafety : AppCompatActivity() {
+    lateinit var lateCar : Car
+
+    class Car(val number : Int) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_null_safety)
+
+        lateCar = Car(10)
+        Log.d("number", "late number" + lateCar.number)
 
         val number: Int = 10
         val number1: Int? = null
@@ -16,10 +24,14 @@ class NullSafety : AppCompatActivity() {
         val number3 = number1?.plus(number)
         Log.d("number", "number3 : $number3")
 
+//        // !! -> 개발자가 null 이 아님을 보장
+//        val number5 : Int = number1!! + 10
+
         // 삼항연산자 -> 엘비스 연산자
         // Null safety 를 위한 도구
         val number4 = number1 ?: 10
         Log.d("number", "number4 : $number4")
+
     }
 
     fun plus(a: Int, b: Int?): Int {
