c49c89db1f3500f808ddde24de7eb83bfe96f326
Yunjae-Na <yunjaena@gmail.com>
2020-09-03 22:39:07 +0900

    Add android fragment second practice
    Add android fragment second practice

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/FragmentActivity.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/FragmentActivity.kt
@@ -3,6 +3,8 @@ package com.yunjaena.androidbasic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
 
@@ -10,6 +12,22 @@ class FragmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         Log.d("life_cycle","onCreate")
+
+        button.setOnClickListener {
+            // 프라그먼트를 동적으로 작동하는 방법
+            val fragmentOne : FragmentOne  = FragmentOne()
+            val fragmentManager : FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+            // 끝을 내는 방법
+            // commit -> 시간 될 때 해(좀더 안정적)
+            // commitNow -> 지금 당장해
+            // commitAllowingStateLoss
+        }
     }
 
     override fun onStart() {
