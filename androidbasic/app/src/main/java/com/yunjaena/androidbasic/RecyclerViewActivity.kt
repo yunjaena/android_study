acc2ff673c2c24edce77ddb60d9dc36131e38199
Yunjae-Na <yunjaena@gmail.com>
2020-09-07 02:17:32 +0900

    Add android RecyclerView second practice
    Add android RecyclerView second practice

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/RecyclerViewActivity.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/RecyclerViewActivity.kt
@@ -1,10 +1,12 @@
 package com.yunjaena.androidbasic
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,21 +24,27 @@ class RecyclerViewActivity : AppCompatActivity() {
         val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this@RecyclerViewActivity))
         recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+        //recycler_view.layoutManager = GridLayoutManager(this@RecyclerViewActivity, 2)
     }
 }
 
 class RecyclerViewAdapter(
     val itemList: ArrayList<CarForList>,
-    val  inflater: LayoutInflater
+    val inflater: LayoutInflater
 ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val carName: TextView
         val carEngine: TextView
 
         init {
             carName = itemView.findViewById(R.id.car_name)
             carEngine = itemView.findViewById(R.id.car_engine)
+            itemView.setOnClickListener{
+                val position : Int = adapterPosition
+                val engineName = itemList.get(position).engine
+                Log.d("engin", engineName)
+            }
         }
     }
 
