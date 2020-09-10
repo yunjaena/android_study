36d3606ccebb32fde6bbbf722766f88212eec742
Yunjae-Na <yunjaena@gmail.com>
2020-09-11 00:22:00 +0900

    Add android network fourth practice
    Add android network fourth practice

--- androidbasic/app/src/main/java/com/yunjaena/androidbasic/NetworkActivity.kt
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/NetworkActivity.kt
@@ -1,10 +1,19 @@
 package com.yunjaena.androidbasic
 
+import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_network.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -15,13 +24,26 @@ class NetworkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
-        NetworkTask().execute()
+        NetworkTask(
+            recycler_person,
+            LayoutInflater.from(this@NetworkActivity)
+            ).execute()
 
     }
 }
 
-class NetworkTask : AsyncTask<Any?, Any?, Any?>() {
-    override fun doInBackground(vararg params: Any?): Any? {
+class NetworkTask(
+    val recyclerView: RecyclerView,
+    val inflater: LayoutInflater
+) : AsyncTask<Any?, Any?, Array<PersonFromServer>>() {
+    override fun onPostExecute(result: Array<PersonFromServer>) {
+        val adapter = PersonAdapter(result, inflater)
+        recyclerView.adapter = adapter
+        super.onPostExecute(result)
+
+    }
+
+    override fun doInBackground(vararg params: Any?): Array<PersonFromServer> {
         var urlString = " http://mellowcode.org/json/students/"
         val url: URL = URL(urlString)
         val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
@@ -31,7 +53,7 @@ class NetworkTask : AsyncTask<Any?, Any?, Any?>() {
 
         var buffer = ""
         if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-            Log.d("conn", "inputstream : " + connection.inputStream)
+            //Log.d("conn", "inputstream : " + connection.inputStream)
             val reader = BufferedReader(
                 InputStreamReader(
                     connection.inputStream,
@@ -42,10 +64,46 @@ class NetworkTask : AsyncTask<Any?, Any?, Any?>() {
             Log.d("conn", "inputstream : " + buffer)
         }
         val data = Gson().fromJson(buffer, Array<PersonFromServer>::class.java)
-        Log.d("conn", "data : " + data)
-        val age = data[0].age
-        Log.d("conn", "age : " + age)
+//        Log.d("conn", "data : " + data)
+//        val age = data[0].age
+//        Log.d("conn", "age : " + age)
 
-        return null
+        return data
+    }
+}
+
+class PersonAdapter(
+    val personList: Array<PersonFromServer>,
+    val inflater: LayoutInflater
+) : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.person_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return personList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.setText(personList.get(position).name ?: "")
+        holder.age.setText(personList.get(position).age.toString() ?: "")
+        holder.intro.setText(personList.get(position).intro ?: "")
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: TextView
+        val age: TextView
+        val intro: TextView
+
+
+        init {
+            name = itemView.findViewById(R.id.person_name)
+            age = itemView.findViewById(R.id.person_age)
+            intro = itemView.findViewById(R.id.person_intro)
+        }
+
     }
 }
