cd67f7d11559dc1dcbd7c9df101032a081b92f2b
Yunjae-Na <yunjaena@gmail.com>
2020-09-10 23:41:32 +0900

    Add android network first practice
    Add android network first practice

---
+++ androidbasic/app/src/main/java/com/yunjaena/androidbasic/NetworkActivity.kt
@@ -0,0 +1,36 @@
+package com.yunjaena.androidbasic
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class NetworkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_network)
+
+        var urlString = " http://mellowcode.org/json/students/"
+        val url: URL = URL(urlString)
+        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+        connection.setRequestProperty("Content_Type", "application/json")
+
+        var buffer = ""
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            Log.d("conn", "inputstream : " + connection.inputStream)
+            val reader = BufferedReader(
+                InputStreamReader(
+                    connection.inputStream,
+                    "UTF-8"
+                )
+            )
+            buffer = reader.readLine()
+        }
+    }
+}
