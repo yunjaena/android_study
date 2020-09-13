25a02d542b4261cb710b7dd71292064eafc2a1fd
Yunjae-Na <yunjaena@gmail.com>
2020-09-14 02:48:33 +0900

    Add android instagram clone coding project
    Add android instagram clone coding project

---
+++ CloneCoding/instagram/src/main/java/com/yunjaena/clonecoding/EmailSignupActivity.kt
@@ -0,0 +1,12 @@
+package com.yunjaena.clonecoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailSignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_signup)
+    }
+}
