df03a6fedb28456e34504701e8fa50f691da313a
Yunjae-Na <yunjaena@gmail.com>
2020-09-14 03:43:16 +0900

    Add application class
    Add application class

--- CloneCoding/instagram/src/main/java/com/yunjaena/clonecoding/EmailSignupActivity.kt
+++ CloneCoding/instagram/src/main/java/com/yunjaena/clonecoding/EmailSignupActivity.kt
@@ -1,12 +1,40 @@
 package com.yunjaena.clonecoding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class EmailSignupActivity : AppCompatActivity() {
+
+    lateinit var userNameView: EditText
+    lateinit var userPassword1View: EditText
+    lateinit var userPassword2View: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_signup)
+
+        initView(this@EmailSignupActivity)
     }
+
+    fun initView(activity: Activity) {
+        userNameView = activity.findViewById(R.id.username_inputbox)
+        userPassword1View = activity.findViewById(R.id.password1_inputbox)
+        userPassword2View = activity.findViewById(R.id.password2_inputbox)
+    }
+
+    fun getUserName(): String {
+        return userNameView.text.toString()
+    }
+
+    fun getUserPassword1(): String {
+        return userPassword1View.text.toString()
+    }
+
+    fun getUserPassword2(): String {
+        return userPassword2View.text.toString()
+    }
+
+
 }
