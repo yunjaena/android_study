f667b425eb15c9d3a701a365b8d716e3aecb1706
Yunjae-Na <yunjaena@gmail.com>
2020-09-25 00:26:27 +0900

    Initialize Todo recycler view
    Initialize Todo recycler view

--- TodoList/app/src/main/java/com/yunjaena/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/yunjaena/todolist/MainActivity.kt
@@ -2,17 +2,27 @@ package com.yunjaena.todolist
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.yunjaena.todolist.databinding.ActivityMainBinding
+import com.yunjaena.todolist.databinding.ItemTodoBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val data = arrayListOf<Todo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        data.add(Todo("숙제", false))
+        data.add(Todo("청소", false))
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = TodoAdapter(data)
     }
 }
 
@@ -21,18 +31,17 @@ data class Todo(val text: String, var isDone: Boolean)
 class TodoAdapter(private val myDataSet: List<Todo>) :
     RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
-    class TodoViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_todo, parent, false)
-        return TodoViewHolder(view)
+        return TodoViewHolder(ItemTodoBinding.bind(view))
     }
 
     override fun getItemCount() = myDataSet.size
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        val textView = holder.view.findViewById<TextView>(R.id.todo_text)
-        textView.text = myDataSet[position].text
+        holder.binding.todoText.text = myDataSet[position].text
     }
 }
