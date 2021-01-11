13084841f27c5f6323ccb99a2ea46d2a91e6bada
Yunjae-Na <yunjaena@gmail.com>
2021-01-11 21:34:00 +0900

    Add RxJava assertNotComplete example
    Add RxJava assertNotComplete example

---
+++ RxJavaJason/src/test/java/com/itvillage/section4/AssertCompleteTest.java
@@ -0,0 +1,27 @@
+package com.itvillage.section4;
+
+import com.itvillage.section03.SampleObservable;
+import com.itvillage.utils.TimeUtil;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * assertComplete를 이용하여 A 지점과 B 지점의 매출 합계 처리가 지정된 시간안에 끝나는지 검증하는 예제
+ */
+public class AssertCompleteTest {
+    @Test
+    public void assertCompleteTest() {
+        SampleObservable.getSalesOfBranchA()
+                .zipWith(
+                        SampleObservable.getSalesOfBranchB(),
+                        (a, b) -> {
+                            TimeUtil.sleep(100L);
+                            return a + b;
+                        }
+                )
+                .test()
+                .awaitDone(3000L, TimeUnit.MILLISECONDS)
+                .assertComplete();
+    }
+}
