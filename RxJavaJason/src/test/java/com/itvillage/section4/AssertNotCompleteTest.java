7aca2e72c1b174f32904c7647b1325dee5caab92
Yunjae-Na <yunjaena@gmail.com>
2021-01-12 19:01:12 +0900

    Add RxJava assertDone example
    Add RxJava assertDone example

---
+++ RxJavaJason/src/test/java/com/itvillage/section4/AssertNotCompleteTest.java
@@ -0,0 +1,27 @@
+package com.itvillage.section4;
+
+import com.itvillage.section03.SampleObservable;
+import com.itvillage.utils.TimeUtil;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * assertNotComplete를 이용하여 A 지점과 B 지점의 매출 합계 처리가 지정된 시간안에 끝나지않았는지 검증하는 예제
+ */
+public class AssertNotCompleteTest {
+    @Test
+    public void assertNotCompleteTest() {
+        SampleObservable.getSalesOfBranchB()
+                .zipWith(
+                        SampleObservable.getSalesOfBranchB(),
+                        (a, b) -> {
+                            TimeUtil.sleep(1000L);
+                            return a + b;
+                        }
+                )
+                .test()
+                .awaitDone(3000L, TimeUnit.MILLISECONDS)
+                .assertNotComplete();
+    }
+}
